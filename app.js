e1666ab16d32ef352eb6327001d0283a83b8c911
shaidavis <shaidavis@gmail.com>
2016-08-23 09:40:47 +0300

    first commit

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost/beers');
+mongoose.connect('mongodb://localhost/beers-auth');
 
 var Beer = require("./models/BeerModel");
 var Review = require("./models/ReviewModel");
+var User = require("./models/UserModel");
 
 var app = express();
 
@@ -14,6 +15,22 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
+
+//------------------------
+// For authentication:
+var passport = require('passport');
+var expressSession = require('express-session');
+
+app.use(expressSession({ secret: 'mySecretKey' }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+//------------------------
+
+
+app.post('/register', passport.authenticate('register'), function (req, res) {
+  res.json(req.user);
+});
 
 app.get('/beers', function (req, res) {
   Beer.find(function (error, beers) {
